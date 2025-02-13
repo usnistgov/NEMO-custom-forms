f5a6bb1e5ab8685545359c61607481bfb111d11b
mrampant <mathieu.rampant@nist.gov>
2025-02-13 12:39:13 -0500

    - updated NEMO and NEMO-CE dependencies to >=7.0.0 in pyproject.toml - added extra dependency check in app readiness logic

--- NEMO_custom_forms/apps.py
+++ NEMO_custom_forms/apps.py
@@ -9,8 +9,14 @@ class CustomFormsConfig(AppConfig):
 
     def ready(self):
         from django.utils.translation import gettext_lazy as _
-        from NEMO.plugins.utils import add_dynamic_notification_types, add_dynamic_email_categories
+        from NEMO.plugins.utils import (
+            add_dynamic_notification_types,
+            add_dynamic_email_categories,
+            check_extra_dependencies,
+        )
         from NEMO_custom_forms.utilities import CUSTOM_FORM_NOTIFICATION, CUSTOM_FORM_EMAIL_CATEGORY
+
+        check_extra_dependencies(self.name, ["NEMO", "NEMO-CE"])
 
         add_dynamic_notification_types(
             [(CUSTOM_FORM_NOTIFICATION, _("Custom forms action - notifies next action candidates"))]
